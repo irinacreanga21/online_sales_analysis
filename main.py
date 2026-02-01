1db177991ed4ff2c385f46e9f06e93f19437b900
Irina Creanga-Stoica <stoica_irina21@yahoo.ca>
2026-02-01 20:13:27 +0200

    I changed qty fr products and delete printing inventory and total inventory calculation lines

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-manager.add_(Product("laptop", 4000.5, 10))
-manager.add_(Product("Phone", 2500.5, 30))
+manager.add_(Product("laptop", 4000.5, 20))
+manager.add_(Product("Phone", 2500.5, 50))
 manager.add_(Product("Tablet", 1000.6, 15))
 
 print("Products from inventory are:")
-manager.display_all()
 
-manager.total_inventory_value()
