f2314aff4a124340c6f45991c4aab66a3d57bde3
Irina Creanga-Stoica <stoica_irina21@yahoo.ca>
2026-02-01 18:54:16 +0200

    Clasa ProductManager_valoare totala inventar

---
+++ product_manager.py
@@ -0,0 +1,22 @@
+
+class ProductManager:
+    def __init__ (self):
+        self.products = []
+        
+    def add_(self, products):
+        self.products.append(products)
+        
+    def display_all(self):
+        for i in self.products:
+            print (f"{i.name}-price:{i.price:.1f}, quantity:{i.quantity}")
+            
+    def total_inventory_value (self):
+        total_value=0.0
+        for i in self.products:
+            total_value = i.price*i.quantity
+            print(f"Total inventory value is: {total_value:.2f}")
+            return total_value
+            
+        
+        
+        
